f5f7cda404acdc3f691c07bd32d6d824fdfa7314
dbeaumont <dbeaumont@google.com>
2020-01-10 07:37:28 -0800

    Adding "callerOf(Class<?>)" to LogSites to allow simpler APIs for logging utilities.
    This avoids people needing to expose "LogSite" as a parameter in logging helper classes and should allow most of the current ~350 call sites using it to be simplified.
    
    RELNOTES=Adding "callerOf(Class<?>)" to LogSites.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=289089749

--- api/src/main/java/com/google/common/flogger/LogSites.java
+++ api/src/main/java/com/google/common/flogger/LogSites.java
@@ -27,15 +27,77 @@ import com.google.common.flogger.backend.Platform;
  */
 public final class LogSites {
   /**
+   * Returns a {@code LogSite} for the caller of the specified class. This can be used in
+   * conjunction with the {@link LoggingApi#withInjectedLogSite(LogSite)} method to implement
+   * logging helper methods. In some platforms, log site determination may be unsupported, and in
+   * those cases this method will always return the {@link LogSite#INVALID} instance.
+   * <p>
+   * For example (in {@code MyLoggingHelper}):
+   * <pre>{@code
+   * public static void logAndSomethingElse(String message, Object... args) {
+   *   logger.atInfo()
+   *       .withInjectedLogSite(callerOf(MyLoggingHelper.class))
+   *       .logVarargs(message, args);
+   * }
+   * }</pre>
+   * <p>
+   * This method should be used for the simple cases where the class in which the logging occurs is
+   * a public logging API. If the log statement is in a different class (not the public logging API)
+   * and the {@code LogSite} instance needs to be passed through several layers, consider using
+   * {@link #logSite()} instead.
+   * <p>
+   * You should also seek to ensure that any API used with this method "looks like a logging API".
+   * It's no good if a log entry contains a class and method name which doesn't correspond to
+   * anything the user can relate to. In particular, the API should probably always accept the log
+   * message or at least some of its parameters, and should always have methods with "log" in their
+   * names to make the connection clear.
+   * <p>
+   * It is very important to note that this method can be very slow, since determining the log site
+   * can involve stack trace analysis. It is only recommended that it is used for cases where
+   * logging is expected to occur (e.g. {@code INFO} level or above). Implementing a helper method
+   * for {@code FINE} logging is usually unnecessary (it doesn't normally need to follow any
+   * specific "best practice" behavior).
+   * <p>
+   * Note that even when log site determination is supported, it is not defined as to whether two
+   * invocations of this method on the same line of code will produce the same instance, equivalent
+   * instances or distinct instance. Thus you should never invoke this method twice in a single
+   * statement (and you should never need to).
+   * <p>
+   * Note that this method call may be replaced in compiled applications via bytecode manipulation
+   * or other mechanisms to improve performance.
+   */
+  public static LogSite callerOf(Class<?> clazz) {
+    // Can't skip anything here since someone could pass in LogSite.class.
+    return Platform.getCallerFinder().findLogSite(clazz, 0);
+  }
+
+  /**
    * Returns a {@code LogSite} for the current line of code. This can be used in conjunction with
    * the {@link LoggingApi#withInjectedLogSite(LogSite)} method to implement logging helper
    * methods. In some platforms, log site determination may be unsupported, and in those cases this
    * method will always return the {@link LogSite#INVALID} instance.
    * <p>
+   * For example (in {@code MyLoggingHelper}):
+   * <pre>{@code
+   * public static void logAndSomethingElse(LogSite logSite, String message, Object... args) {
+   *   logger.atInfo()
+   *       .withInjectedLogSite(logSite)
+   *       .logVarargs(message, args);
+   * }
+   * }</pre>
+   * where callers would do:
+   * <pre>{@code
+   * MyLoggingHelper.logAndSomethingElse(logSite(), "message...");
+   * }</pre>
+   * <p>
+   * Because this method adds an additional parameter and exposes a Flogger specific type to the
+   * calling code, you should consider using {@link #callerOf(Class<?>)} for simple logging
+   * utilities.
+   * <p>
    * It is very important to note that this method can be very slow, since determining the log site
    * can involve stack trace analysis. It is only recommended that it is used for cases where
-   * logging is expected to occur (e.g. {@code WARNING} level or above). Implementing a helper
-   * method for {@code FINE} logging is usually unnecessary (it doesn't normally need to follow any
+   * logging is expected to occur (e.g. {@code INFO} level or above). Implementing a helper method
+   * for {@code FINE} logging is usually unnecessary (it doesn't normally need to follow any
    * specific "best practice" behavior).
    * <p>
    * Note that even when log site determination is supported, it is not defined as to whether two
@@ -47,6 +109,7 @@ public final class LogSites {
    * or other mechanisms to improve performance.
    */
   public static LogSite logSite() {
+    // Don't call "callerOf()" to avoid making another stack entry.
     return Platform.getCallerFinder().findLogSite(LogSites.class, 0);
   }
 
