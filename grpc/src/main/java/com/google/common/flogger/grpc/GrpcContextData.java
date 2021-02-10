79957c8a8894a34ce1a3f0c7bfb4d974bada9199
dbeaumont <dbeaumont@google.com>
2021-02-10 12:57:23 -0800

    Releasing gRPC context implementation. Enable by including the new "flogger-grpc-context" artifact and setting the "flogger.logging_context" system property to:   com.google.common.flogger.grpc.GrpcContextDataProvider#getInstance Eventually it is hoped that this will be auto-configured via the class path, but for now a system property is required.
    RELNOTES=Open-sourcing the gRPC context implementation.
    PiperOrigin-RevId: 356806259

---
+++ grpc/src/main/java/com/google/common/flogger/grpc/GrpcContextData.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2019 The Flogger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.flogger.grpc;
+
+import com.google.common.flogger.context.LogLevelMap;
+import com.google.common.flogger.context.ScopeMetadata;
+import com.google.common.flogger.context.Tags;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
+
+/** A mutable thread-safe holder for context scoped logging information. */
+final class GrpcContextData {
+  static GrpcContextData create(@NullableDecl GrpcContextData parent) {
+    return new GrpcContextData(parent);
+  }
+
+  static Tags getTagsFor(@NullableDecl GrpcContextData context) {
+    if (context != null) {
+      Tags tags = context.tagRef.get();
+      if (tags != null) {
+        return tags;
+      }
+    }
+    return Tags.empty();
+  }
+
+  static ScopeMetadata getMetadataFor(@NullableDecl GrpcContextData context) {
+    if (context != null) {
+      ScopeMetadata metadata = context.metadataRef.get();
+      if (metadata != null) {
+        return metadata;
+      }
+    }
+    return ScopeMetadata.none();
+  }
+
+  static boolean shouldForceLoggingFor(
+      @NullableDecl GrpcContextData context, String loggerName, Level level) {
+    if (context != null) {
+      LogLevelMap map = context.logLevelMapRef.get();
+      if (map != null) {
+        return map.getLevel(loggerName).intValue() <= level.intValue();
+      }
+    }
+    return false;
+  }
+
+  private abstract static class ScopedReference<T> {
+    private final AtomicReference<T> value;
+
+    ScopedReference(@NullableDecl T initialValue) {
+      this.value = new AtomicReference<>(initialValue);
+    }
+
+    @NullableDecl
+    final T get() {
+      return value.get();
+    }
+
+    // Note: If we could use Java 1.8 runtime libraries, this would just be "accumulateAndGet()",
+    // but gRPC is Java 1.7 compatible: https://github.com/grpc/grpc-java/blob/master/README.md
+    final void mergeFrom(@NullableDecl T delta) {
+      if (delta != null) {
+        T current;
+        do {
+          current = get();
+        } while (!value.compareAndSet(current, current != null ? merge(current, delta) : delta));
+      }
+    }
+
+    abstract T merge(T current, T delta);
+  }
+
+  private final ScopedReference<Tags> tagRef;
+  private final ScopedReference<ScopeMetadata> metadataRef;
+  private final ScopedReference<LogLevelMap> logLevelMapRef;
+
+  private GrpcContextData(@NullableDecl GrpcContextData parent) {
+    this.tagRef =
+        new ScopedReference<Tags>(parent != null ? parent.tagRef.get() : null) {
+          @Override
+          Tags merge(Tags current, Tags delta) {
+            return current.merge(delta);
+          }
+        };
+    this.metadataRef =
+        new ScopedReference<ScopeMetadata>(parent != null ? parent.metadataRef.get() : null) {
+          @Override
+          ScopeMetadata merge(ScopeMetadata current, ScopeMetadata delta) {
+            return current.concatenate(delta);
+          }
+        };
+    this.logLevelMapRef =
+        new ScopedReference<LogLevelMap>(parent != null ? parent.logLevelMapRef.get() : null) {
+          @Override
+          LogLevelMap merge(LogLevelMap current, LogLevelMap delta) {
+            return current.merge(delta);
+          }
+        };
+  }
+
+  void addTags(@NullableDecl Tags tags) {
+    tagRef.mergeFrom(tags);
+  }
+
+  void addMetadata(@NullableDecl ScopeMetadata metadata) {
+    metadataRef.mergeFrom(metadata);
+  }
+
+  void applyLogLevelMap(@NullableDecl LogLevelMap logLevelMap) {
+    if (logLevelMap != null) {
+      // Set the global flag to trigger testing of the log level map from now on (we only apply a
+      // log level map to an active context or one that's about to become active).
+      GrpcContextDataProvider.INSTANCE.setLogLevelMapFlag();
+      logLevelMapRef.mergeFrom(logLevelMap);
+    }
+  }
+}
---
+++ grpc/src/main/java/com/google/common/flogger/grpc/GrpcScopedLoggingContext.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2019 The Flogger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.flogger.grpc;
+
+import static com.google.common.flogger.util.Checks.checkNotNull;
+
+import com.google.common.flogger.MetadataKey;
+import com.google.common.flogger.context.LogLevelMap;
+import com.google.common.flogger.context.ScopeMetadata;
+import com.google.common.flogger.context.ScopedLoggingContext;
+import com.google.common.flogger.context.Tags;
+import com.google.errorprone.annotations.CheckReturnValue;
+import io.grpc.Context;
+
+/**
+ * A gRPC context based implementation of Flogger's scoped logging context API. This is a lazily
+ * loaded singleton instance returned from {@link GrpcContextDataProvider#getContextApiSingleton()}
+ * which provides application code with a mechanism for controlling logging contexts.
+ *
+ * <p>It is vital that this class is lazily loaded (rather than being loaded when the logging
+ * platform is configured) since other classes it uses may well use fluent loggers.
+ */
+final class GrpcScopedLoggingContext extends ScopedLoggingContext {
+  private static final ScopedLoggingContext INSTANCE = new GrpcScopedLoggingContext();
+
+  static ScopedLoggingContext getGrpcConfigInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  @CheckReturnValue
+  public ScopedLoggingContext.Builder newScope() {
+    return new ScopedLoggingContext.Builder() {
+      @Override
+      public LoggingScope install() {
+        GrpcContextData newContextData =
+            GrpcContextData.create(GrpcContextDataProvider.currentContext());
+        newContextData.addTags(getTags());
+        newContextData.addMetadata(getMetadata());
+        newContextData.applyLogLevelMap(getLogLevelMap());
+        return installContextData(newContextData);
+      }
+    };
+  }
+
+  private static LoggingScope installContextData(GrpcContextData newContextData) {
+    // Capture these variables outside the lambda.
+    Context context =
+        Context.current().withValue(GrpcContextDataProvider.getContextKey(), newContextData);
+    @SuppressWarnings("MustBeClosedChecker")
+    Context prev = context.attach();
+    return () -> context.detach(prev);
+  }
+
+  @Override
+  public boolean addTags(Tags tags) {
+    checkNotNull(tags, "tags");
+    GrpcContextData context = GrpcContextDataProvider.currentContext();
+    if (context != null) {
+      context.addTags(tags);
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public <T> boolean addMetadata(MetadataKey<T> key, T value) {
+    // Serves as the null pointer check, and we don't care much about the extra allocation in the
+    // case where there's no context, because that should be very rare (and the singleton is small).
+    ScopeMetadata metadata = ScopeMetadata.singleton(key, value);
+    GrpcContextData context = GrpcContextDataProvider.currentContext();
+    if (context != null) {
+      context.addMetadata(metadata);
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean applyLogLevelMap(LogLevelMap logLevelMap) {
+    checkNotNull(logLevelMap, "log level map");
+    GrpcContextData context = GrpcContextDataProvider.currentContext();
+    if (context != null) {
+      context.applyLogLevelMap(logLevelMap);
+      return true;
+    }
+    return false;
+  }
+}
